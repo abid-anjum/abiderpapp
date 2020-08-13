3724e89856e100812693225cdc913fe954b5f58b
abidhussain <abid786@gmail.com>
2020-08-13 11:17:28 +0300

    first commit

---
+++ src/main/java/com/abid/erp/solution/AbidErpSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.abid.erp.solution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbidErpSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbidErpSolutionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abid/erp/solution/AbidErpSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abid.erp.solution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbidErpSolutionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
